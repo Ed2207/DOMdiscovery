d9aace348e15eab7e33d95f63c7d80dd054e3960
Ed <polet.edouard@gmail.com>
2022-10-17 11:27:08 +0200

    Introduction done

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-console.log(document.title);
-document.title = "Modifying the DOM";
-console.log(document.title);
-// document.body.style.backgroundColor = "#FF69B4";
-document.body.style.backgroundColor = "rgb(134,34,56)";
-// console.log(document.body.children)
-const n = document.body.children
-for (const enfants of n) {
-    console.log (enfants)
-}
-
+// DOM intro
+// console.log(document.title);
+// document.title = "Modifying the DOM";
+// console.log(document.title);
+// // document.body.style.backgroundColor = "#FF69B4";
+// let a = Math.floor (Math.random ()* 254)
+// let b = Math.floor (Math.random ()* 254)
+// let c = Math.floor (Math.random ()* 254)
+// document.body.style.backgroundColor = `rgb(${a},${b},${c})`;
+// // console.log(document.body.children)
+// const n = document.body.children
+// for (const enfants of n) {
+//     console.log (enfants)
+// }
+// DOM Selector
 
